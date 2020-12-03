5f6590770081da7ca7041d0cc8f9b117596a0761
David Ferreira de Almeida <72570193+da-ferreira@users.noreply.github.com>
2020-12-03 16:45:58 -0300

    Update pilha.py

--- Estruturas de Dados/pilha.py
+++ Estruturas de Dados/pilha.py
@@ -1,50 +1,54 @@
 """
 Autor: David Ferreira de Almeida
-
 Implemetação da estrutura de dados Pilha
 """
 
-def stack():
-    """ Cria a pilha """
-    return []
+class Stack:
+    def __init__(self):
+        self.stack = []
+        self.size = 0
 
 
-def push(stack, item):
-    """ Adiciona o elemento "item" no topo da pilha """
-    stack.append(item)
+    # Adiciona um elemento no topo da pilha
+    def push(self, elemento):
+        self.stack.append(elemento)
+        self.size += 1
 
 
-def pop(stack):
-    """ Remove o elemento que esta no topo da pilha """
-
-    if len(stack) > 0:
-        return stack.pop()
-    
-    raise IndexError('A pilha esta vazia')
-
-
-def peek(stack):
-    """ Mostra elemento o elemento que esta no topo da pilha """
-
-    if len(stack) > 0:
-        return stack[-1]
-    
-    raise IndexError('A pilha esta vazia')
+    # Remove o elemento do topo da pilha
+    def pop(self):
+        if self.size > 0:
+            self.size -= 1
+            return self.stack.pop()
+            
+        raise IndexError('A pilha esta vazia')
 
 
-# Demostracao basica do funcionamento da pilha
+    def peek(self):
+        if self.size > 0:
+            return self.stack[-1]
+
+        raise IndexError('A pilha esta vazia')
+
+
+    def __str__(self):
+        return ' -> '.join(str(x) for x in self.stack)
+
+
 if __name__ == '__main__':
-    pilha = stack()
+    pilha = Stack()
 
-    for i in range(1, 11, 3):
-        push(pilha, i)
+    pilha.push(15)
+    pilha.push(100)
+    pilha.push('Casa')
+    pilha.push(7.5)
 
     print(pilha)
-    print(pop(pilha))    
-    
-    push(pilha, 'casa')
-    print(pilha)
 
-    print(peek(pilha))
-    print(pop(pilha))
-    print(pop(pilha))
+    print(pilha.pop())
+    print(pilha.pop())
+
+    print(pilha.peek())
+
+    print(pilha)
+  
